e12ce33e385661025c101fbcf528506af5852690
Kanoa Haley <kanoa@Kanoa-MBP.local>
2016-10-30 14:37:28 -0400

    initial commit

---
+++ desktop/src/ca/kanoa/zombieworld/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ca.kanoa.zombieworld.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ca.kanoa.zombieworld.ZombieWorldGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ZombieWorldGame(), config);
+	}
+}
