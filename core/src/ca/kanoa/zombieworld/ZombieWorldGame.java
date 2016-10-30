4f420da7d1a9cd06a270e8ce274b97a07620f0eb
Kanoa Haley <kanoa@kanoa.ca>
2016-10-30 15:00:10 -0400

    changed colour

--- core/src/ca/kanoa/zombieworld/ZombieWorldGame.java
+++ core/src/ca/kanoa/zombieworld/ZombieWorldGame.java
@@ -23,7 +23,7 @@ public class ZombieWorldGame extends OrganizedApplicationAdapter {
 
     @Override
     public void renderGame() {
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(1, 1, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
         batch.draw(img, 10, 10);
