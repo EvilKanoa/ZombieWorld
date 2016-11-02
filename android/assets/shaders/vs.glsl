7ad3502e293836e2b5f93a5b820003cfbabeb4d5
BotJonathan <imhungry3.14159@gmail.com>
2016-11-02 00:18:19 -0400

    Added code to shaders

--- android/assets/shaders/vs.glsl
+++ android/assets/shaders/vs.glsl
@@ -1,10 +1,17 @@
 layout (location = 0) in vec3 position;
 layout (location = 1) in vec3 inNormal;
+layout (location = 2) in vec2 inTexCoord;
 
 out vec3 normal;
+out vec2 texCoord;
+
+uniform mat4 world;
+uniform mat4 view;
+uniform mat4 projection;
 
 void main()
 {
-    gl_Position = vec4(position, 1.0);
-    ourColor = color; // Set ourColor to the input color we got from the vertex data
+    gl_Position = projection * view * world * vec4(position, 1.0f);
+    normal = (vec4(inNormal, 1.0f) * world).xyz;
+    texCoord = vec2(inTexCoord.x, 1.0f - inTexCoord.y);
 }
