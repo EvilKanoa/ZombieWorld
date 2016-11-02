059296169337a74133c01c6e6d4a63cddf498345
Kanoa Haley <kanoa@kanoa.ca>
2016-11-01 22:47:50 -0400

    added shader compiler

---
+++ core/src/ca/kanoa/zombieworld/graphics/ShaderLoader.java
@@ -0,0 +1,14 @@
+package ca.kanoa.zombieworld.graphics;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
+
+public class ShaderLoader {
+
+    public static ShaderProgram compile(String vertexShader, String fragmentShader) {
+        String vertexData = Gdx.files.internal("shaders/" + vertexShader).readString();
+        String fragmentData = Gdx.files.internal("shaders/" + fragmentShader).readString();
+        return new ShaderProgram(vertexShader, fragmentShader);
+    }
+
+}
