a8242c72dc2b29ec96db3c322c493614fbf642e0
BotJonathan <imhungry3.14159@gmail.com>
2017-01-16 12:57:37 -0500

    updated shader

---
+++ android/assets/shaders/texturedSpriteVS.glsl
@@ -0,0 +1,13 @@
+#version 110
+attribute vec3 inPosition;
+attribute vec2 inTexCoord;
+
+varying vec2 texCoord;
+
+uniform mat4 projWorld;
+
+void main()
+{
+    texCoord = inTexCoord;
+    gl_Position = projWorld * vec4(inPosition, 1.0);
+}
